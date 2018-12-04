bd7cefc252347ed4c64509fc2de91ff1dfe50996
hkstwk <hkolvoort+github@gmail.com>
2018-12-04 21:52:58 +0100

    SImpleHttpRequest improvements, like reset button and some conditional showing showing of div's

--- src/app/simple-http/simple-http.component.ts
+++ src/app/simple-http/simple-http.component.ts
@@ -9,27 +9,37 @@ import {FormGroup, FormBuilder} from "@angular/forms";
 })
 export class SimpleHttpComponent implements OnInit {
   data: Object;
-  loading: boolean;
+  loading: boolean = false;
   myForm: FormGroup;
 
   constructor(private http: HttpClient, fb: FormBuilder) {
     this.myForm = fb.group({
       'url': ['https://jsonplaceholder.typicode.com/posts']
     });
-    this.loading = false;
   }
 
   ngOnInit() {
   }
 
   makeRequest(url: string): void {
+    if (this.data) this.data = null;
     this.loading = true;
     this.http
       .get(url)
-      .subscribe(data => {
+      .subscribe(
+        (data: any) => {
         this.data = data;
         this.loading = false;
+      },
+      (err: any) => {
+        this.data = err;
+        this.loading = false;
       });
+  }
+
+  reset(): void {
+    this.loading = false;
+    this.data = null;
   }
 
 }
