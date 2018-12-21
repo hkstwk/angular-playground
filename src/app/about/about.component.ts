a0bd291066a49254374b80bc1b232732b2024e52
hkstwk <hkolvoort+github@gmail.com>
2018-12-21 23:26:45 +0100

    working on individual streams per user

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import {MessageService} from "./message.service";
 import {Message} from "../model/message.model";
-import {Subscription, Observable, from, fromEvent, merge} from "rxjs";
+import {Subscription, Observable, from, fromEvent, merge, range, OperatorFunction} from "rxjs";
 import {map} from "rxjs/operators";
 import {HttpClient} from "@angular/common/http";
 import {mergeMap, debounceTime, buffer, filter, startWith} from "rxjs/operators";
@@ -18,7 +18,6 @@ export class AboutComponent implements OnInit {
   @ViewChild('refreshBtn') refreshBtn : ElementRef<any>;
 
   doubleClickMessage: string;
-  doubleClickMessage2: string;
   noDoubleClickMessage: string;
 
   messageStream: Subscription;
@@ -26,16 +25,9 @@ export class AboutComponent implements OnInit {
   index: number = 0;
 
   githubUsers: GithubUser[] = new Array<GithubUser>();
-
-  requestStream: Observable<any> = from(["https://api.github.com/users"]);
-
-  response: GithubUser[] = new Array<GithubUser>();
-  responseStream = this.requestStream
-    .pipe(
-      mergeMap( requestUrl => {
-        return this.http.get(requestUrl.toString());
-      })
-    );
+  userSuggestion1: GithubUser = new GithubUser();
+  userSuggestion2: GithubUser = new GithubUser();
+  userSuggestion3: GithubUser = new GithubUser();
 
   constructor(private messageService: MessageService, private http: HttpClient) {
     this.messageStream = this.messageService.getCurrentMessage()
@@ -46,30 +38,63 @@ export class AboutComponent implements OnInit {
         () => { console.log("onError") },
         () => { console.log("onComplete") }
         );
-
-    this.responseStream.subscribe(
-      (response : any) => {
-        this.githubUsers = response;
-      }
-    )
   }
 
   ngOnInit() {
     const rxRefreshBtn = this.refreshBtn.nativeElement;
-    const refresh$ = fromEvent(rxRefreshBtn, 'click').pipe(
+    const refreshClick$ = fromEvent(rxRefreshBtn, 'click');
+    const refreshClickClear$ = refreshClick$.pipe(
+      map( (resp: Observable<any>) => {
+        return "hallo" }));
+    const request$ = refreshClick$.pipe(
       startWith('startup click'),
-      map( (event: any) => {
+      map( () => {
         let randomOffset = Math.floor(Math.random()*500);
         return 'https://api.github.com/users?since=' + randomOffset;
-      }),
+      }));
+
+    const response$ = request$.pipe(
       mergeMap( requestUrl => {
         return this.http.get(requestUrl.toString());
-      }))
-      .subscribe((resp: GithubUser[]) => {
-        console.log(resp);
-        this.response = resp;
+      }));
+
+    response$.subscribe((resp: GithubUser[]) => {
         this.githubUsers = resp;
       });
+
+    const suggestion1$ = response$.pipe(
+      map((listUsers: GithubUser[]) => {
+        // get one random user from the list
+        return listUsers[Math.floor(Math.random()*listUsers.length)];
+      }));
+    suggestion1$.subscribe((user: GithubUser) => {
+      console.log(user);
+      this.userSuggestion1 = user;
+    });
+
+    const suggestion2$ = response$.pipe(
+      // startWith(''),
+      map((listUsers: GithubUser[]) => {
+        // get one random user from the list
+        return listUsers[Math.floor(Math.random()*listUsers.length)];
+      }));
+
+    suggestion2$.subscribe((user: GithubUser) => {
+      console.log(user);
+      this.userSuggestion2 = user;
+    });
+
+    const suggestion3$ = response$.pipe(
+      startWith(''),
+      map((listUsers: GithubUser[]) => {
+        // get one random user from the list
+        return listUsers[Math.floor(Math.random()*listUsers.length)];
+      }));
+
+    suggestion3$.subscribe((user: GithubUser) => {
+      console.log(user);
+      this.userSuggestion3 = user;
+    });
 
     const toLength = a => a.length;
     const rxBtn = this.btn.nativeElement;                           // get the button element
@@ -79,18 +104,6 @@ export class AboutComponent implements OnInit {
     const clickCount$ = buffered$.pipe(map(toLength));              // map buffered$ to #clicks
     const doubleClick$ = clickCount$.pipe(filter(x => x === 2));    // only double clicks
     const notDoubleClick$ = clickCount$.pipe(filter(x => x !== 2)); // only the not double clicks
-
-    const doubleClick2$ = click$.pipe(
-      buffer(click$.pipe(debounceTime(250))),
-      map((a: any) => toLength(a)),
-      filter(x => x === 2)
-      )
-      .subscribe(
-        () => {
-          this.doubleClickMessage2 = "Double click 2";
-          this.noDoubleClickMessage = "";
-        }
-      )
 
     doubleClick$.subscribe(
       () => {
@@ -109,7 +122,6 @@ export class AboutComponent implements OnInit {
       () => {
         this.noDoubleClickMessage = "Not a double click";
         this.doubleClickMessage="";
-        this.doubleClickMessage2="";
       }
     )
 
@@ -128,7 +140,6 @@ export class AboutComponent implements OnInit {
   clear() {
     this.index = 0;
     this.doubleClickMessage="";
-    this.doubleClickMessage2="";
     this.noDoubleClickMessage="";
     this.messageService.clearCurrentMessage();
   }
