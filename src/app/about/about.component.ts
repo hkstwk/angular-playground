419fdc7b90fec2ac675d6416ea210046cf00a976
hkstwk <hkolvoort+github@gmail.com>
2018-12-29 22:21:52 +0100

    closing and refreshing from suggestion streams working. Found out that each suggestion has it's own http request and set of 30 users. When closing a user with the x button a new one is selected from this set of 30. No new call is done. Every now and then same user will pop up obviously.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit, ViewChild, AfterViewInit} from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
 import {MessageService} from "./message.service";
 import {Message} from "../model/message.model";
-import {Subscription, fromEvent, merge, interval, combineLatest} from "rxjs";
-import {map, mergeMap, debounceTime, buffer, filter, startWith, take} from "rxjs/operators";
+import {Subscription, fromEvent, merge, combineLatest} from "rxjs";
+import {map, mergeMap, debounceTime, buffer, filter, startWith} from "rxjs/operators";
 import {HttpClient} from "@angular/common/http";
 import {GithubUser} from "../model/github-user.model";
 
@@ -11,16 +11,13 @@ import {GithubUser} from "../model/github-user.model";
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit, AfterViewInit {
+export class AboutComponent implements OnInit {
 
   @ViewChild('btn') btn;
   @ViewChild('refreshButton') refreshButton;
   @ViewChild('closeButton1') closeButton1;
   @ViewChild('closeButton2') closeButton2;
   @ViewChild('closeButton3') closeButton3;
-
-  rxCloseButton1: any;
-  closeClick1$: any;
 
   doubleClickMessage: string;
   noDoubleClickMessage: string;
@@ -29,7 +26,7 @@ export class AboutComponent implements OnInit, AfterViewInit {
   message: Message;
   index: number = 0;
 
-  githubUsers: GithubUser[];// = new Array<GithubUser>();
+  githubUsers: GithubUser[];
   userSuggestion1: GithubUser;
   userSuggestion2: GithubUser;
   userSuggestion3: GithubUser;
@@ -54,7 +51,6 @@ export class AboutComponent implements OnInit, AfterViewInit {
      */
     const rxRefreshButton = this.refreshButton.nativeElement;
     const refreshClick$ = fromEvent(rxRefreshButton, 'click');
-    const refreshClickNull$ = refreshClick$.pipe(map( () => { return null; } ));
 
 
     /** request stream, initially "faking" startup click,
@@ -68,134 +64,63 @@ export class AboutComponent implements OnInit, AfterViewInit {
         return 'https://api.github.com/users?since=' + randomOffset;
       }));
 
-    /** response stream, using URL of returned by the request stream to
+    request$.subscribe((resp: string) => { console.log(resp)});
+
+    /** response stream, using URL returned by the request stream to
      * call Github API. mergeMap is used to flatten out the JSON that
      * is returned by HttpClient in yet another Observable.
      */
     const response$ = request$.pipe(
       mergeMap( requestUrl => {
-        return this.http.get(requestUrl.toString());
+        var resp = this.http.get(requestUrl.toString());
+        resp.subscribe((resp) => console.log(resp));
+        return resp;
       }));
-
-    response$.subscribe(
-      (resp) => { console.log("API called. ", resp) }
-    )
-
-    // const helper$ = this.closeClick1$.pipe(
-    //   combineLatest(response$,
-    //     (click, listUsers) => {
-    //       return listUsers[Math.floor(Math.random()*listUsers.length)];
-    //    })
-    //   );
 
     /**
      * Suggestion1 Stream is for first GitHub user to display.
      * Using some Math functions we randomly select a GitHub user
-     * from the response stream. Then merge with the null stream
-     * to stop rendering first user later on.
+     * from the already available response stream.
      */
+    const rxCloseButton1 = this.closeButton1.nativeElement;
+    const closeClick1$ = fromEvent(rxCloseButton1, 'click').pipe(startWith(null));
     const suggestion1$ = merge(
-      // first part to merge
-      response$.pipe(
-        map((listUsers: GithubUser[]) => {
+      combineLatest(
+        closeClick1$,
+        response$,
+        (click, listUsers: GithubUser[]) => {
           // get one random user from the list
           return listUsers[Math.floor(Math.random()*listUsers.length)];
         })
-      ),
-      // second part to merge
-      refreshClickNull$)
-      // initial null so that nothing is rendered until we have a user to display
-      .pipe(startWith(null));
+      ,refreshClick$.pipe(map(function(){ return null; }))
+    );
 
     /** render suggestion1
      *  when null nothing will be displayed. When user is returned it will be displayed
      *  (handled using *ngIf in HTML)
      */
     suggestion1$.subscribe((user: GithubUser) => {
-      this.closeClick1$ = null;
       console.log(user);
       this.userSuggestion1 = user;
-      setTimeout(() => {
-        if (!this.closeClick1$) {
-          this.rxCloseButton1 = this.closeButton1.nativeElement;
-          this.closeClick1$ = fromEvent(this.rxCloseButton1, 'click');
-          this.closeClick1$.subscribe(
-             (resp) => { console.log(resp); }
-           );
-        }
-      }
-      , 1000);
     });
 
-    const source1$ = interval(100).pipe(
-      map(function (i) { return 'First: ' + i; })
-    );
-
-    const source2$ = interval(151).pipe(
-      map(function (i) { return 'Second: ' + i; })
-    );
-
-    const source$ = combineLatest(
-      source1$,
-      source2$,
-      (s1, s2) => { return s1 + '; ' + s2 }
-    ).pipe(
-      take(10));
-
-    source$.subscribe(
-      function (x) {
-        console.log('Next: %s', x);
-      },
-      function (err) {
-        console.log('Error: %s', err);
-      },
-      function () {
-        console.log('Completed');
-      });
-
+    /**
+     * Suggestion2 Stream is for second GitHub user to display.
+     * Using some Math functions we randomly select a GitHub user
+     * from the already available response stream.
+     */
     const rxCloseButton2 = this.closeButton2.nativeElement;
-    const closeClick2$ = fromEvent(rxCloseButton2, 'click');
-
-    /**
-     * Suggestion2 Stream is for first GitHub user to display.
-     * Using some Math functions we randomly select a GitHub user
-     * from the response stream. Then merge with the null stream
-     * to stop rendering first user later on.
-     */
-    const suggestion2$temp = combineLatest(
-      // first part to merge
-      closeClick2$.pipe(startWith(null)),
-      response$,
-      (click, listUsers: GithubUser[]) => {
-          // get one random user from the list
-          return listUsers[Math.floor(Math.random()*listUsers.length)];
-        })
-    // initial null so that nothing is rendered until we have a user to display
-
+    const closeClick2$ = fromEvent(rxCloseButton2, 'click').pipe(startWith(null));
     const suggestion2$ = merge(
-      suggestion2$temp,
-      refreshClick$.pipe(map(function(){ return null; })
-        // startWith(null))
-      ))
-
-    /**
-     * Suggestion2 Stream is for first GitHub user to display.
-     * Using some Math functions we randomly select a GitHub user
-     * from the response stream. Then merge with the null stream
-     * to stop rendering first user later on.
-     */
-    const suggestion2a$ = merge(
-      // first part to merge
-      response$.pipe(
-        map((listUsers: GithubUser[]) => {
+      combineLatest(
+        closeClick2$,
+        response$,
+        (click, listUsers: GithubUser[]) => {
           // get one random user from the list
           return listUsers[Math.floor(Math.random()*listUsers.length)];
-        })
-      ),
-      // second part to merge
-      refreshClickNull$)
-    // initial null so that nothing is rendered until we have a user to display
-      .pipe(startWith(null));
+        }),
+      refreshClick$.pipe(map(function(){ return null; })
+      ));
 
     /** render suggestion2
      *  when null nothing will be displayed. When user is returned it will be displayed
@@ -207,23 +132,22 @@ export class AboutComponent implements OnInit, AfterViewInit {
     });
 
     /**
-     * Suggestion3 Stream is for first GitHub user to display.
+     * Suggestion2 Stream is for third GitHub user to display.
      * Using some Math functions we randomly select a GitHub user
-     * from the response stream. Then merge with the null stream
-     * to stop rendering first user later on.
+     * from the already available response stream.
      */
+    const rxCloseButton3 = this.closeButton3.nativeElement;
+    const closeClick3$ = fromEvent(rxCloseButton3, 'click').pipe(startWith(null));
     const suggestion3$ = merge(
-        // first part to merge
-        response$.pipe(
-          map((listUsers: GithubUser[]) => {
-            // get one random user from the list
-            return listUsers[Math.floor(Math.random()*listUsers.length)];
-          })
-        ),
-        // second part to merge
-        refreshClickNull$)
-      // initial null so that nothing is rendered until we have a user to display
-        .pipe(startWith(null));
+      combineLatest(
+        closeClick3$,
+        response$,
+        (click, listUsers: GithubUser[]) => {
+          // get one random user from the list
+          return listUsers[Math.floor(Math.random()*listUsers.length)];
+        }),
+      refreshClick$.pipe(map(function(){ return null; })
+      ));
 
     /** render suggestion3
      *  when null nothing will be displayed. When user is returned it will be displayed
@@ -264,45 +188,6 @@ export class AboutComponent implements OnInit, AfterViewInit {
     );
 
   };
-
-  ngAfterViewInit() {
-
-    /** Close 1 close this user and replace by new one
-     *  close this user and replace by new one from latest response stream.
-     *  it will only pick from latest response stream using that one as cache
-     */
-    setTimeout(() => {
-      // this.rxCloseButton1 = this.closeButton1.nativeElement;
-      // const closeClick1$ = fromEvent(this.rxCloseButton1, 'click');
-      // closeClick1$.subscribe(
-      //   (resp) => { console.log(resp); }
-      // );
-    }, 1000);
-
-    /** Close 2 close this user and replace by new one
-     *  close this user and replace by new one from latest response stream.
-     *  it will only pick from latest response stream using that one as cache
-     */
-    setTimeout(() => {
-      const rxCloseButton2 = this.closeButton2.nativeElement;
-      const closeClick2$ = fromEvent(rxCloseButton2, 'click');
-      closeClick2$.subscribe(
-        (resp) => { console.log(resp); }
-      );
-    }, 1000);
-
-    /** Close 3 close this user and replace by new one
-     *  close this user and replace by new one from latest response stream.
-     *  it will only pick from latest response stream using that one as cache
-     */
-    setTimeout(() => {
-      const rxCloseButton3 = this.closeButton3.nativeElement;
-      const closeClick3$ = fromEvent(rxCloseButton3, 'click');
-      closeClick3$.subscribe(
-        (resp) => { console.log(resp); }
-      );
-    }, 1000);
-  }
 
   add() {
     this.index += 1;
