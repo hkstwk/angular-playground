6066f8a5d911c010380b77a571feb5384bf4c914
hkstwk <hkolvoort+github@gmail.com>
2019-01-03 19:12:25 +0100

    message service  updated

--- src/app/service/messages.service.ts
+++ src/app/service/messages.service.ts
@@ -3,7 +3,13 @@ import {Subject, Observable} from "rxjs";
 import {User} from "../model/user.model";
 import {Thread} from "../model/thread.model";
 import {ChatMessage} from "../model/chat-message.model";
-import {filter} from "rxjs/internal/operators";
+import {filter, scan, publishReplay, refCount, map} from "rxjs/internal/operators";
+
+const initialChatMessages:ChatMessage[]=[];
+
+interface IChatMessagesOperation extends Function {
+  (chatMessages: ChatMessage[]): ChatMessage[];
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +18,35 @@ export class MessagesService {
   // a stream that publishes new messages only once
   newChatMessages: Subject<ChatMessage> = new Subject<ChatMessage>();
 
-  constructor() { }
+  messages: Observable<ChatMessage[]>;
+
+  // `updates` receives _operations_ to be applied to our `messages`
+  // it's a way we can perform changes on *all* messages (that are currently // stored in `messages`)
+  updates: Subject<any> = new Subject<any>();
+
+  // action streams
+  create: Subject<ChatMessage> = new Subject<ChatMessage>();
+
+  constructor() {
+    this.messages = this.updates.pipe(
+      // watch the updates and accumulate operations on the messages
+      scan((chatMessages: ChatMessage[],
+             operation: IChatMessagesOperation) => {
+          return operation(chatMessages) },
+        initialChatMessages),
+      // make sure we can share the most recent list of messages across anyone
+      publishReplay(1),
+      refCount()
+    );
+
+    this.create.pipe(
+      map(function(chatMessage: ChatMessage): IChatMessagesOperation {
+        return (chatMessages: ChatMessage[]) => {
+          return chatMessages.concat(chatMessage);
+        };
+      })
+    ).subscribe(this.updates);
+  }
 
   addMessage(chatMessage: ChatMessage): void {
     this.newChatMessages.next(chatMessage);
@@ -25,6 +59,8 @@ export class MessagesService {
         return (chatMessage.thread.id === thread.id) &&
           // and isn't authored by this user
           (chatMessage.author.id !== user.id);
-    }))
+      }))
   }
+
+
 }
