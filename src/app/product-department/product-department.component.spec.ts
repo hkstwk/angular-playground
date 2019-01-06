fd009dec54d19dd76ceea216d88321402e377d0c
hkstwk <hkolvoort+github@gmail.com>
2019-01-06 21:13:25 +0100

    jasmine unit tests for product department and for product image.

--- src/app/product-department/product-department.component.spec.ts
+++ src/app/product-department/product-department.component.spec.ts
@@ -1,25 +1,72 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductDepartmentComponent } from './product-department.component';
+import { Product } from "../model/product.model";
+import { Component } from "@angular/core";
+import {Article} from "../article/article.model";
 
 describe('ProductDepartmentComponent', () => {
-  let component: ProductDepartmentComponent;
-  let fixture: ComponentFixture<ProductDepartmentComponent>;
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
+
+  @Component({
+    selector: `host-component`,
+    template: `<product-department [product]="product"></product-department>`
+  })
+  class TestHostComponent {
+    product: Product;
+
+    setProduct(_product: Product) {
+      this.product = _product;
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductDepartmentComponent ]
+      declarations: [ ProductDepartmentComponent, TestHostComponent ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProductDepartmentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show "" (nothing)', () => {
+    let product = new Product(
+      'MYSHOES',
+      'Black Running Shoes',
+      '../assets/images/products/black-shoes.jpg',
+      [], // <-- empty product department
+      109.99);
+    testHostComponent.setProduct(product);
+    testHostFixture.detectChanges();
+    expect(testHostFixture.nativeElement.querySelector('div#productDept').innerText).toEqual("");
   });
+
+  it('should show "Women > Apparel > Jackets & Vests"', () => {
+    let product = new Product(
+      'NEATOJACKET',
+      'Blue Jacket',
+      '../assets/images/products/blue-jacket.jpg',
+      ['Women', 'Apparel', 'Jackets & Vests'], // <-- product department subject to test
+      238.99);
+    testHostComponent.setProduct(product);
+    testHostFixture.detectChanges();
+    expect(testHostFixture.nativeElement.querySelector('div#productDept').innerText).toEqual("Women > Apparel > Jackets & Vests");
+  });
+
+  it('should show "Men > Accessories > Hats"', () => {
+    let product  = new Product(
+      'NICEHAT',
+      'A nice black hat',
+      '../assets/images/products/black-hat.jpg',
+      ['Men','Accessories','Hats'], // <-- product department subject to test
+      29.99);
+    testHostComponent.setProduct(product);
+    testHostFixture.detectChanges();
+    expect(testHostFixture.nativeElement.querySelector('div#productDept').innerText).toEqual("Men > Accessories > Hats");
+  });
+
 });
--- src/app/product-image/product-image.component.spec.ts
+++ src/app/product-image/product-image.component.spec.ts
@@ -1,25 +1,61 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductImageComponent } from './product-image.component';
+import { Product } from "../model/product.model";
+import { Component } from "@angular/core";
 
 describe('ProductImageComponent', () => {
-  let component: ProductImageComponent;
-  let fixture: ComponentFixture<ProductImageComponent>;
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
+  const host: string = "http://localhost:9876";
+
+  @Component({
+    selector: `host-component`,
+    template: `<product-image [product]="product"></product-image>`
+  })
+  class TestHostComponent {
+    product: Product;
+
+    setProduct(_product: Product) {
+      this.product = _product;
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductImageComponent ]
+      declarations: [ ProductImageComponent, TestHostComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProductImageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have an product image', () => {
+    let product = new Product(
+      'MYSHOES',
+      'Black Running Shoes',
+      '../assets/images/products/black-shoes.jpg',
+      [],
+      109.99);
+    testHostComponent.setProduct(product);
+    testHostFixture.detectChanges();
+    console.log(testHostFixture.nativeElement.querySelector('img#productImg'));
+    expect(testHostFixture.nativeElement.querySelector('img#productImg').toBe);
   });
+
+  it('should show product image source "..../assets/images/products/black-hat.jpg"', () => {
+    let product  = new Product(
+      'NICEHAT',
+      'A nice black hat',
+      '../assets/images/products/black-hat.jpg',
+      ['Men','Accessories','Hats'],
+      29.99);
+    testHostComponent.setProduct(product);
+    testHostFixture.detectChanges();
+    expect(testHostFixture.nativeElement.querySelector('img#productImg').src).toEqual(host + '/assets/images/products/black-hat.jpg');
+  });
+
 });
