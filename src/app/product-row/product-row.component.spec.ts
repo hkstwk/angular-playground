12b739e488924485fb8adc5bc04760f3c2d6abea
hkstwk <hkolvoort+github@gmail.com>
2019-01-07 22:34:28 +0100

    Addes simple test for product row component

--- src/app/product-row/product-row.component.spec.ts
+++ src/app/product-row/product-row.component.spec.ts
@@ -1,25 +1,64 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductRowComponent } from './product-row.component';
+import {Component} from "@angular/core";
+import {Product} from "../model/product.model";
+import {ProductDepartmentComponent} from "../product-department/product-department.component";
+import {ProductImageComponent} from "../product-image/product-image.component";
+import {PriceDisplayComponent} from "../price-display/price-display.component";
 
 describe('ProductRowComponent', () => {
-  let component: ProductRowComponent;
-  let fixture: ComponentFixture<ProductRowComponent>;
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
+  const host: string = "http://localhost:9876";
+
+  @Component({
+    selector: `host-component`,
+    template: `<product-row 
+      [product]="product"
+      (click)='clicked(product)'
+      >
+</product-row>`
+  })
+  class TestHostComponent {
+    product: Product;
+
+    setProduct(_product: Product) {
+      this.product = _product;
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductRowComponent ]
+      declarations: [
+        ProductRowComponent,
+        ProductDepartmentComponent,
+        ProductImageComponent,
+        PriceDisplayComponent,
+        TestHostComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProductRowComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show product name BLACK RUNNING SHOES', () => {
+    let product = new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '../assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99);
+    testHostComponent.setProduct(product);
+    testHostFixture.detectChanges();
+    expect(testHostFixture.nativeElement.querySelector('div#productName').innerText).toEqual("Black Running Shoes");
+    expect(testHostFixture.nativeElement.querySelector('div#productSKU').innerText).toEqual("SKU #MYSHOES");
+    expect(testHostFixture.nativeElement.querySelector('product-department#productDept').innerText).toEqual("Men > Shoes > Running Shoes\n");
+    // expect(testHostFixture.nativeElement.querySelector('product-image#productImg').innerHTML).toEqual(host + '/assets/images/products/black-hat.jpg');
+
+
   });
 });
