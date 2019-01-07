203bfd32600983c920471c8c56dbbcef20c79164
hkstwk <hkolvoort+github@gmail.com>
2019-01-07 17:37:36 +0100

    Added simple test for price-display component.

--- src/app/price-display/price-display.component.spec.ts
+++ src/app/price-display/price-display.component.spec.ts
@@ -1,25 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PriceDisplayComponent } from './price-display.component';
+import { Product } from "../model/product.model";
+import { Component } from "@angular/core";
 
 describe('PriceDisplayComponent', () => {
-  let component: PriceDisplayComponent;
-  let fixture: ComponentFixture<PriceDisplayComponent>;
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
+
+  @Component({
+    selector: `host-component`,
+    template: `<price-display [price]="price"></price-display>`
+  })
+  class TestHostComponent {
+    price: number;
+
+    setPrice(_price: number) {
+      this.price = _price;
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PriceDisplayComponent ]
+      declarations: [ PriceDisplayComponent, TestHostComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PriceDisplayComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show product price of 109.99', () => {
+    let price = 109.99;
+    testHostComponent.setPrice(price);
+    testHostFixture.detectChanges();
+    expect(testHostFixture.nativeElement.querySelector('div#priceDisplay').innerText).toEqual('109.99');
   });
 });
