fa1afda6936cb235168d971af827b201d170a6e3
harm kolvoort <hkolvoort+github@gmail.com>
2019-01-12 12:01:41 +0100

    updated function chatMessagesForThreadUser and accompanying tests.

--- src/app/service/chat-messages.service.spec.ts
+++ src/app/service/chat-messages.service.spec.ts
@@ -4,6 +4,7 @@ import {User} from "../model/user.model";
 import {Thread} from "../model/thread.model";
 import {ChatMessage} from "../model/chat-message.model";
 import {TestBed} from "@angular/core/testing";
+import {Observable} from "rxjs";
 
 describe('ChatMessagesService',() => {
 
@@ -17,19 +18,46 @@ describe('ChatMessagesService',() => {
 
   it('should test', () => {
 
-    const user: User = new User('Harm', '');
-    const thread: Thread = new Thread('t1', 'Harm', '');
+    const user1: User = new User('Harm', '');
+    const user2: User = new User('Bea', '');
+    const thread1: Thread = new Thread('t1', 'Thread Harm', '');
+
 
     const m1: ChatMessage = new ChatMessage({
-      author: user,
-      text: 'Hi!',
-      thread: thread
+      author: user2,
+      text: ` => 1  Hi! this is a message from ${user2.name} on ${thread1.name}`,
+      thread: thread1
     });
 
     const m2: ChatMessage = new ChatMessage({
-      author: user,
-      text: 'Bye!',
-      thread: thread
+      author: user1,
+      text: ` => 2  Bye! from ${user1.name} on ${thread1.name}`,
+      thread: thread1
+    });
+
+    const m3: ChatMessage = new ChatMessage({
+      author: user2,
+      text: `=> 3  Hi! this is a message from ${user2.name} on ${thread1.name}`,
+      thread: thread1
+    });
+
+    const m4: ChatMessage = new ChatMessage({
+      author: user1,
+      text: `=> 4  Bye! from ${user1.name} on ${thread1.name}`,
+      thread: thread1
+    });
+
+
+    const m5: ChatMessage = new ChatMessage({
+      author: user1,
+      text: `=> 5  Hi! this is a message from ${user1.name} on ${thread1.name}`,
+      thread: thread1
+    });
+
+    const m6: ChatMessage = new ChatMessage({
+      author: user1,
+      text: `=> 6  Bye! from ${user1.name} on ${thread1.name}`,
+      thread: thread1
     });
 
     const chatMessagesService: ChatMessagesService = new ChatMessagesService();
@@ -46,8 +74,18 @@ describe('ChatMessagesService',() => {
         console.log('=> chatMessages: ' + chatMessages.length);
       });
 
+    // listen to the stream of chatMessages for thread user
+    chatMessagesService.chatMessagesForThreadUser(thread1, user1)
+        .subscribe( (chatMessage: ChatMessage) => {
+          console.log('=> chatMessagesForThreadUser: ' + chatMessage.text);
+        });
+
     chatMessagesService.addMessage(m1);
     chatMessagesService.addMessage(m2);
+    chatMessagesService.addMessage(m3);
+    chatMessagesService.addMessage(m4);
+    chatMessagesService.addMessage(m5);
+    chatMessagesService.addMessage(m6);
 
   });
 });
