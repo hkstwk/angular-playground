4cf4431bbb8ba6e9803f0ea7a0417e37ed11c705
hkstwk <hkolvoort+github@gmail.com>
2019-01-28 09:02:47 +0100

    small update to get e2e test green again. FIrst steps ever with protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-playground!');
+    expect(page.getParagraphText()).toEqual('ng-book2 on Angular 7');
   });
 });
