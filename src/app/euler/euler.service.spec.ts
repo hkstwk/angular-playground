71ba9dfe3c2f0e0b529ed5e4f2c6f0bbff1ad21d
hkstwk <hkolvoort+github@gmail.com>
2019-02-24 09:15:14 +0100

    mock test euler service

--- src/app/euler/euler.service.spec.ts
+++ src/app/euler/euler.service.spec.ts
@@ -1,18 +1,77 @@
 import {TestBed} from "@angular/core/testing";
 import {EulerService} from "./euler.service";
-import {HttpClient, HttpHandler} from "@angular/common/http";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {Euler001Request} from "./euler001request";
+import {inject} from "@angular/core/testing";
+import {Euler001Response} from "./euler001response";
 
 describe('EulerService', () => {
 
-    beforeEach(() => TestBed.configureTestingModule({
-        providers: [
-            EulerService,
-            HttpClient,
-            HttpHandler],
-    }));
+    let service: EulerService;
+    let httpMock: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                EulerService
+            ],
+            imports: [
+                HttpClientTestingModule
+            ],
+        });
+
+        service = TestBed.get(EulerService);
+        httpMock = TestBed.get(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
+    });
 
     it('should be created', () => {
-        const service: EulerService = TestBed.get(EulerService);
         expect(service).toBeTruthy();
     });
+
+    it('should do a POST request to the Euler001 API',
+        inject([HttpTestingController, EulerService],
+            (httpMock: HttpTestingController, service: EulerService) => {
+
+            let reqBody = { multiple1: 3, multiple2: 5, limit: 100 };
+
+            // We call the service
+            service.getEuler001(reqBody).subscribe();
+
+            // We set the expectations for the HttpClient mock
+            const req = httpMock.expectOne('http://localhost:8080/EulerWebService/api/euler/1');
+            expect(req.request.method).toEqual('POST');
+
+        })
+    );
+
+    it('should return a proper json euler001response body',
+        inject([HttpTestingController, EulerService],
+            (httpMock: HttpTestingController, service: EulerService) => {
+
+            // Set the request body for the call
+            let reqBody = { multiple1: 3, multiple2: 5, limit: 100 };
+
+            // We call the service using this request body
+            service.getEuler001(reqBody).subscribe((data: Euler001Response) => {
+                console.log(data);
+                expect(data.sum).toBe(123456789);
+                expect(data.multiple1).toBe(987);
+                expect(data.multiple2).toBe(654);
+                expect(data.limit).toBe(321);
+            });
+
+            // We set the expectations for the HttpClient mock
+            const req = httpMock.expectOne('http://localhost:8080/EulerWebService/api/euler/1');
+            expect(req.request.method).toEqual('POST');
+            expect(req.request.responseType).toEqual('json');
+
+            // Then we set the fake data to be returned by the mock
+            req.flush({ multiple1: 987, multiple2: 654, limit: 321, sum: 123456789});
+
+            })
+    );
 });
--- src/app/product-row/product-row.component.spec.ts
+++ src/app/product-row/product-row.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProductRowComponent } from './product-row.component';
+import {ProductRowComponent} from './product-row.component';
 import {Component} from "@angular/core";
 import {Product} from "../model/product.model";
 import {ProductDepartmentComponent} from "../product-department/product-department.component";
@@ -8,88 +8,89 @@ import {ProductImageComponent} from "../product-image/product-image.component";
 import {PriceDisplayComponent} from "../price-display/price-display.component";
 
 describe('ProductRowComponent', () => {
-  let testHostComponent: TestHostComponent;
-  let testHostFixture: ComponentFixture<TestHostComponent>;
-  const host: string = "http://localhost:9876";
+    let testHostComponent: TestHostComponent;
+    let testHostFixture: ComponentFixture<TestHostComponent>;
+    const host: string = "http://localhost:9876";
 
-  @Component({
-    selector: `host-component`,
-    template: `<product-row 
+    @Component({
+        selector: `host-component`,
+        template: `<product-row 
       [product]="product"
       (click)='clicked(product)'
       >
 </product-row>`
-  })
-  class TestHostComponent {
-    product: Product;
-
-    setProduct(_product: Product) {
-      this.product = _product;
-    }
-  };
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        ProductRowComponent,
-        ProductDepartmentComponent,
-        ProductImageComponent,
-        PriceDisplayComponent,
-        TestHostComponent ]
     })
-    .compileComponents();
-  }));
+    class TestHostComponent {
+        product: Product;
 
-  beforeEach(() => {
-    testHostFixture = TestBed.createComponent(TestHostComponent);
-    testHostComponent = testHostFixture.componentInstance;
-  });
+        setProduct(_product: Product) {
+            this.product = _product;
+        }
+    }
+    ;
 
-  it('should show product department "Men > Shoes > Running Shoes\n"', () => {
-    let product = new Product(
-        'MYSHOES',
-        'Black Running Shoes',
-        '../assets/images/products/black-shoes.jpg',
-        ['Men', 'Shoes', 'Running Shoes'],
-        109.99);
-    testHostComponent.setProduct(product);
-    testHostFixture.detectChanges();
-    expect(testHostFixture.nativeElement.querySelector('product-department#productDept').innerText).toEqual("Men > Shoes > Running Shoes");
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                ProductRowComponent,
+                ProductDepartmentComponent,
+                ProductImageComponent,
+                PriceDisplayComponent,
+                TestHostComponent]
+        })
+            .compileComponents();
+    }));
 
-  it('should show product name BLACK RUNNING SHOES', () => {
-    let product = new Product(
-      'MYSHOES',
-      'Black Running Shoes',
-      '../assets/images/products/black-shoes.jpg',
-      ['Men', 'Shoes', 'Running Shoes'],
-      109.99);
-    testHostComponent.setProduct(product);
-    testHostFixture.detectChanges();
-    expect(testHostFixture.nativeElement.querySelector('div#productName').innerText).toEqual("Black Running Shoes");
-  });
+    beforeEach(() => {
+        testHostFixture = TestBed.createComponent(TestHostComponent);
+        testHostComponent = testHostFixture.componentInstance;
+    });
 
-  it('should show "SKU #MYSHOES"', () => {
-    let product = new Product(
-      'MYSHOES',
-      'Black Running Shoes',
-      '../assets/images/products/black-shoes.jpg',
-      ['Men', 'Shoes', 'Running Shoes'],
-      109.99);
-    testHostComponent.setProduct(product);
-    testHostFixture.detectChanges();
-    expect(testHostFixture.nativeElement.querySelector('div#productSKU').innerText).toEqual("SKU #MYSHOES");
-  });
+    it('should show product department "Men > Shoes > Running Shoes\n"', () => {
+        let product = new Product(
+            'MYSHOES',
+            'Black Running Shoes',
+            '../assets/images/products/black-shoes.jpg',
+            ['Men', 'Shoes', 'Running Shoes'],
+            109.99);
+        testHostComponent.setProduct(product);
+        testHostFixture.detectChanges();
+        expect(testHostFixture.nativeElement.querySelector('product-department#productDept').innerText).toEqual("Men > Shoes > Running Shoes");
+    });
 
-  it('should have image src "../assets/images/products/black-shoes.jpg" in innerHTML', () => {
-    let product = new Product(
-      'MYSHOES',
-      'Black Running Shoes',
-      '../assets/images/products/black-shoes.jpg',
-      ['Men', 'Shoes', 'Running Shoes'],
-      109.99);
-    testHostComponent.setProduct(product);
-    testHostFixture.detectChanges();
-    expect(testHostFixture.nativeElement.querySelector('product-image#productImg').innerHTML).toContain('../assets/images/products/black-shoes.jpg');
-  });
+    it('should show product name BLACK RUNNING SHOES', () => {
+        let product = new Product(
+            'MYSHOES',
+            'Black Running Shoes',
+            '../assets/images/products/black-shoes.jpg',
+            ['Men', 'Shoes', 'Running Shoes'],
+            109.99);
+        testHostComponent.setProduct(product);
+        testHostFixture.detectChanges();
+        expect(testHostFixture.nativeElement.querySelector('div#productName').innerText).toEqual("Black Running Shoes");
+    });
+
+    it('should show "SKU #MYSHOES"', () => {
+        let product = new Product(
+            'MYSHOES',
+            'Black Running Shoes',
+            '../assets/images/products/black-shoes.jpg',
+            ['Men', 'Shoes', 'Running Shoes'],
+            109.99);
+        testHostComponent.setProduct(product);
+        testHostFixture.detectChanges();
+        expect(testHostFixture.nativeElement.querySelector('div#productSKU').innerText).toEqual("SKU #MYSHOES");
+    });
+
+    it('should have image src "../assets/images/products/black-shoes.jpg" in innerHTML', () => {
+        let product = new Product(
+            'MYSHOES',
+            'Black Running Shoes',
+            '../assets/images/products/black-shoes.jpg',
+            ['Men', 'Shoes', 'Running Shoes'],
+            109.99);
+        testHostComponent.setProduct(product);
+        testHostFixture.detectChanges();
+        expect(testHostFixture.nativeElement.querySelector('product-image#productImg').innerHTML).toContain('../assets/images/products/black-shoes.jpg');
+    });
 });
